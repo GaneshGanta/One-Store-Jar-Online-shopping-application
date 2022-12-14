517fbef916c17d746e1104d9956c73757fde95db
NITESH DILIP MESHRAM <101390539+nilmes@users.noreply.github.com>
2022-12-14 23:38:11 +0530

    Update Servic.java

--- src/main/java/com/onestore/service/Servic.java
+++ src/main/java/com/onestore/service/Servic.java
@@ -11,7 +11,7 @@ public class Servic {
 	CustomerDao dao;
 	
 	public void register(Customer cust) {
-		Customer obj = dao.save(cust);
-		System.out.println(obj.toString());
+		Customer obj1 = dao.save(cust);
+		System.out.println(obj1.toString());
 	}
 }
