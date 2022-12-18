a110b6991c59617d3306aa7de523873a8f4eedba
GaneshGanta <ganeshsagar373@gmail.com>
2022-12-18 18:15:24 +0530

    day5 work

--- src/main/java/com/onestore/SpringProjectApplication.java
+++ src/main/java/com/onestore/SpringProjectApplication.java
@@ -2,6 +2,11 @@ package com.onestore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+
 
 @SpringBootApplication
 public class SpringProjectApplication {
@@ -9,5 +14,13 @@ public class SpringProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringProjectApplication.class, args);
 	}
+	
+	@Bean
+	public LocalValidatorFactoryBean validator(MessageSource ms) {
+			LocalValidatorFactoryBean lvfb=new LocalValidatorFactoryBean();
+			lvfb.setValidationMessageSource(ms);
+			return lvfb;
+	}
+	
 
 }
